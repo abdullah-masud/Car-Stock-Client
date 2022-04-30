11f28c7147d242577e5fe571366b5774773ee3e4
Abdullah Al Masud <masudctg00@gmail.com>
2022-05-01 01:51:32 +0600

    taken email and pass using react ref

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
 
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
+
 
     const handleLogin = (event) => {
         event.preventDefault();
-        console.log('hero alom')
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password)
     }
 
     return (
@@ -17,12 +22,12 @@ const Login = () => {
             <Form onSubmit={handleLogin} className='w-50 mx-auto '>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control type="email" ref={emailRef} placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control type="password" ref={passwordRef} placeholder="Password" required />
                 </Form.Group>
                 <Button className='mx-auto d-block navbar-login-button' type='submit' variant="outline-light">Login</Button>
             </Form>
