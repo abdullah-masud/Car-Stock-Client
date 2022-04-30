b8db55b0403113740b66bc954a59a36e6899e96e
Abdullah Al Masud <masudctg00@gmail.com>
2022-05-01 05:48:20 +0600

    toast added for reset password

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,17 +1,53 @@
 import React, { useRef } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
 
 const Login = () => {
     const emailRef = useRef('')
     const passwordRef = useRef('')
 
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+    const location = useLocation()
+    let from = location.state?.from?.pathname || "/";
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    if (loading) {
+        return <Loading />
+    }
+
+    let errorElement;
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message} </p>
+    }
+
     const handleLogin = (event) => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(email, password)
+        signInWithEmailAndPassword(email, password);
+    }
+
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Email Sent');
+        } else {
+            toast('Please enter email address');
+        }
     }
 
     return (
@@ -29,15 +65,17 @@ const Login = () => {
                 </Form.Group>
                 <Button className='mx-auto d-block navbar-login-button' type='submit' variant="outline-light">Login</Button>
             </Form>
+            <div className='mx-auto w-50'>{errorElement}</div>
             <div className=" w-50 mx-auto">
                 <p className='mt-3'>
                     New to Car Stock? <Link to="/register">Create an account</Link>
                 </p>
                 <p className='mt-3'>
-                    Forget Password?<button className='btn btn-link text-primary text-decoration-none'>Reset Password</button>
+                    Forget Password?<button onClick={resetPassword} className='btn btn-link text-primary text-decoration-none'>Reset Password</button>
                 </p>
             </div>
             <SocialLogin />
+            <ToastContainer />
         </Container >
     );
 };
